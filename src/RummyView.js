7f865f0b5b209fb420030cb135a82bfc5bb2599d
Kyle Smith <kyle@smithsrus.com>
2014-02-12 11:39:04 -0500

    Refactor display code

--- src/RummyView.js
+++ src/RummyView.js
@@ -7,15 +7,34 @@ RummyView.prototype.hand = function() {
   return this.game.player(this.player).cards;
 };
 
-window.onload = function() {
-  var view = new RummyView(1);
-  var cardsList = document.getElementById("hand");
-  
-  var hand = view.hand();
-  
-  hand.forEach(function(card) {
+RummyView.prototype.discardPile = function() {
+  return this.game.discardPile();
+};
+
+RummyView.prototype.displayCards = function(cards, element) {
+  cards.forEach(function(card) {
     var li = document.createElement("li");
     li.innerText = card.rank() + "-" + card.suit();
-    cardsList.appendChild(li);
+    element.appendChild(li);
   });
+};
+
+RummyView.prototype.displayHand = function() {
+  var cardsList = document.getElementById("hand");
+  var hand = this.hand();
+  this.displayCards(hand, cardsList);
+};
+
+RummyView.prototype.displayDiscardPile = function() {
+  var discardPileList = document.getElementById("discard");
+  discardPileList.innerText = "";
+  
+  var cards = this.discardPile().cards();
+  this.displayCards(cards, discardPileList);
+};
+
+window.onload = function() {
+  var view = new RummyView(1);
+  view.displayHand();
+  view.displayDiscardPile();
 }
