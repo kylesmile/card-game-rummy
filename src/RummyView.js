8d56515b66d60c223ff5380fb9212d156e53b57f
Kyle Smith <kyle@smithsrus.com>
2014-02-14 11:00:15 -0500

    Make view display card backs for opponent hand

--- src/RummyView.js
+++ src/RummyView.js
@@ -22,7 +22,17 @@ RummyView.prototype.displayCards = function(cards, element) {
   });
 };
 
-RummyView.prototype.displayHand = function() {
+RummyView.prototype.updateOpponentHand = function() {
+  var cardCount = this.game.player(2).cards().length;
+  var cardList = $("#opponent-hand");
+  cardList.find('li').remove();
+  for(var i = 0; i < cardCount; i++) {
+    var li = $('<li><img src="images/cards/backs_blue.png"/></li>');
+    cardList.append(li);
+  }
+};
+
+RummyView.prototype.updateHand = function() {
   var cardsList = $("#hand");
   cardsList.find('li').remove();
   var hand = this.hand();
@@ -30,26 +40,28 @@ RummyView.prototype.displayHand = function() {
     var li = $('<li data-index="' + index + '"><img src="images/cards/' + imageName(card) + '"/></li>');
     li.click(this, function(clickEvent) {
       clickEvent.data.game.discard($(this).data('index'));
-      clickEvent.data.displayHand();
+      clickEvent.data.updateView();
       clickEvent.data.displayDiscardPile();
     });
     cardsList.append(li);
   }, this);
 };
 
-RummyView.prototype.displayDeck = function() {
+RummyView.prototype.updateDeck = function() {
   var deckSection = $(".deck-discard");
-  var deck = $('<img></img>');
-  deck.addClass('deck');
-  deck.attr('src', 'images/cards/backs_blue.png');
-  deck.click(this, function(clickEvent) {
-    clickEvent.data.game.draw();
-    clickEvent.data.displayHand();
-  });
-  deckSection.append(deck)
+  if (deckSection.find('.deck').length == 0) {
+    var deck = $('<img></img>');
+    deck.addClass('deck');
+    deck.attr('src', 'images/cards/backs_blue.png');
+    deck.click(this, function(clickEvent) {
+      clickEvent.data.game.draw();
+      clickEvent.data.updateView();
+    });
+    deckSection.append(deck)
+  }
 }
 
-RummyView.prototype.displayDiscardPile = function() {
+RummyView.prototype.updateDiscardPile = function() {
   var discardPileSection = $(".deck-discard");
   discardPileSection.find('ul').remove();
   var discardPileList = $('<ul class="discard"></ul>');
@@ -60,9 +72,14 @@ RummyView.prototype.displayDiscardPile = function() {
   discardPileSection.append(discardPileList);
 };
 
+RummyView.prototype.updateView = function() {
+  this.updateOpponentHand();
+  this.updateDeck();
+  this.updateDiscardPile();
+  this.updateHand();
+};
+
 $(document).ready(function() {
   var view = new RummyView(1);
-  view.displayHand();
-  view.displayDeck();
-  view.displayDiscardPile();
+  view.updateView();
 });
